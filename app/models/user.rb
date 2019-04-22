2da8f80cf56e1850438f76fd12d8b757f869a501
Daniel Hidalgo G <daniel.hg94@gmail.com>
2019-04-22 19:20:49 -0400

    CRUD Category & associatons created

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   belongs_to :role
   has_many :evaluations
+  has_many :categories
 
   validates_presence_of %i[name last_name]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_004627) do
+ActiveRecord::Schema.define(version: 2019_04_22_231447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "evaluations", force: :cascade do |t|
     t.string "name"
@@ -53,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_04_07_004627) do
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "evaluations", "users"
   add_foreign_key "users", "roles"
 end
