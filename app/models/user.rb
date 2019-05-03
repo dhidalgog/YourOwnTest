17b9faa0ae84799dca3da8d003beadeaaa4ba72a
Daniel Hidalgo G <daniel.hg94@gmail.com>
2019-05-03 17:34:38 -0400

    Facebook login added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   belongs_to :role
   has_many :evaluations
@@ -14,7 +15,16 @@ class User < ApplicationRecord
   has_many :belongs_tos
   has_many :courses, :through => :belongs_tos
 
-  validates_presence_of %i[name last_name]
+  validates_presence_of %i[name]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.role_id = 2
+    end
+  end
 
   def admin?
     self.role.name == "Admin"
@@ -27,4 +37,6 @@ class User < ApplicationRecord
   def student?
     self.role.name == "Student"
   end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
   resources :categories
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    :omniauth_callbacks => "users/omniauth_callbacks"
+  }
 
   resources :questions_to_evaluate, only: :destroy
   resources :evaluations
