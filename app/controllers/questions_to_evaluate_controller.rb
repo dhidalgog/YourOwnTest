d03a65cfbedda931e59ba938fe3586d553206801
Daniel Hidalgo G <daniel.hg94@gmail.com>
2019-05-05 02:20:32 -0400

    evaluation views design changed

--- app/controllers/questions_to_evaluate_controller.rb
+++ app/controllers/questions_to_evaluate_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsToEvaluateController < ApplicationController
-
+skip_authorization_check
   def destroy
     @qe = QuestionsToEvaluate.find(params[:id])
     @evaluation = Evaluation.find(@qe.evaluation_id)
